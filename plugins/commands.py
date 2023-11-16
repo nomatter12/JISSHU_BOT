ec531b7f15760fd99d5743b23e15bf4fa9600b86
nomatter12 <150418296+nomatter12@users.noreply.github.com>
2023-11-16 22:57:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -59,7 +59,7 @@ async def start(client, message):
                     InlineKeyboardButton('🎉 Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ 🎉', url=CHNL_LNK)
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgUAAxkBAAEKVaxlCWGs1Ri6ti45xliLiUeweCnu4AACBAADwSQxMYnlHW4Ls8gQMAQ") 
+        m=await message.reply_sticker("CAACAgIAAxkBAAIqlGVWUN649BxqXx1IeaHS6mYT4vS3AAKjAQACEBptIkfOxfML2NdjHgQ") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
